1ac6a87651436b308a9910a06fcecdfeece3aec8
Christian Torrico Avila <christorricoavila@gmail.com>
2021-04-19 21:17:31 -0400

    source code

--- Web App/ViewController.swift
+++ Web App/ViewController.swift
@@ -1,19 +1,31 @@
-//
-//  ViewController.swift
-//  Web App
-//
-//  Created by user on 4/19/21.
-//
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
 
+    
+    @IBOutlet var myWebView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let url = URL(string: "https://www.youtube.com")
+        myWebView.load(URLRequest(url: url!))
     }
 
-
+    @IBAction func youtubePage(_ sender: Any) {
+        let url = URL(string: "https://www.youtube.com")
+        myWebView.load(URLRequest(url: url!))
+    }
+    
+    @IBAction func fbPage(_ sender: Any) {
+        let url = URL(string: "https://www.fb.com")
+        myWebView.load(URLRequest(url: url!))
+    }
+    
+    @IBAction func yahooPage(_ sender: UIButton) {
+        let url = URL(string: "https://www.yahoo.com")
+        myWebView.load(URLRequest(url: url!))
+    }
 }
 
